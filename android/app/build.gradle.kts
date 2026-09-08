2fd43abdfa4d49a561cd4d23c3136b1df97409b3
Navnath <wadghanenavnath29@gmail.com>
2026-09-08 23:37:40 +0530

    Release CopyPast v1.0.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.copypast"
         minSdk = 23
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
